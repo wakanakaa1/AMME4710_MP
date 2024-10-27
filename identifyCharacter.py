79e7a9a9005edf52131330ca6466e617429bde74
wakanakaa1 <awakankar11@gmail.com>
2024-10-27 15:57:58 +1100

    Adding Machine Learning

--- identifyCharacter.py
+++ identifyCharacter.py
@@ -1,2 +1,47 @@
 # This file is used as the main file to identify the characters. 
 
+import os 
+import identifyCharacterFunction as functions
+
+# Select a range of characters
+bothCannonImage = 'Characters/bothCannon/360.png'
+bothKnightImage = 'Characters/bothKnight/360.png'
+bothRookImage = 'Characters/bothRook/360.png'
+greenElephantImage = 'Characters/greenElephant/360.png'
+greenKingImage = 'Characters/greenKing/360.png'
+greenPawnImage = 'Characters/greenPawn/360.png'
+redElephantImage = 'Characters/redElephant/360.png'
+redGuardImage = 'Characters/redGaurd/360.png'
+redKingImage = 'Characters/redKing/360.png'
+redPawnImage = 'Characters/redPawn/1.png'
+
+# The name of the folder each image is located in
+folder = [
+    (bothCannonImage, "Both Cannon"),
+    (bothKnightImage, "Both Knight"),
+    (bothRookImage, "Both Rook"),
+    (greenElephantImage, "Green Elephant"),
+    (greenKingImage, "Green King"),
+    (greenPawnImage, "Green Pawn"),
+    (redElephantImage, "Red Elephant"),
+    (redGuardImage, "Red Guard"),
+    (redKingImage, "Red King"),
+    (redPawnImage, "Red Pawn")
+]
+
+print("Unpacking Classifer File")
+modelFile = 'BestClassifier.pkl'
+model, label = functions.loadSVM(modelFile)
+print("Finished Unpacking")
+
+for imagePath, folderName in folder:
+
+    processedImage = functions.preProcessing(imagePath)
+    HOGFeatures = functions.extractHOG(processedImage)
+
+    topPredictions = functions.predictKLabel(model, HOGFeatures, label, k=3)
+
+    print(f"\nImage: {os.path.basename(imagePath)}")
+    print("Top predictions (ordered by confidance):")
+    for scene, confidance in topPredictions:
+        print(f"Scene: {scene}, Confidence: {confidance:.4f}")
--- identifyCharacterFunction.py
+++ identifyCharacterFunction.py
@@ -1,1 +1,54 @@
 # This file is used to store the functions that will be used in the identifications of the processed images. 
+# Import libraries
+import pickle, cv2, numpy as np
+
+def loadSVM(data):
+    file = open(data, 'rb')
+    model, label = pickle.load(file)
+    file.close()
+
+    return model, label
+
+def predictKLabel(SVMModel, HOGFeatures, label, k=3):
+
+    # Predict class probabilities
+    classProbability = SVMModel.predict_proba(HOGFeatures)[0]
+
+    # Get top-K indecies sorted by its confidance values
+    topKIndex = np.argsort(classProbability)[::-1][:k]
+
+    # Retrieve class labels and probability
+    topKLabel = label.inverse_transform(topKIndex)
+    topKConfidance = classProbability[topKIndex]
+
+    return list(zip(topKLabel, topKConfidance))
+
+def preProcessing(imagePath):
+
+    # Read image into function
+    image = cv2.imread(imagePath)
+
+    # Resize and grayscale
+    size = (128, 128)
+    resizedImage = cv2.resize(image, size)
+    grayImage = cv2.cvtColor(resizedImage, cv2.COLOR_BGR2GRAY)
+
+    return grayImage
+
+def extractHOG(grayImage):
+
+    # Set parameters
+    winSize = (128, 128)
+    blockSize = (16, 16)
+    blockStride = (8, 8)
+    cellSize = (8, 8)
+    bins = 9
+
+    # Create HOG descriptor and features
+    HOGDescriptor = cv2.HOGDescriptor(winSize, blockSize, blockStride, cellSize, bins)
+    features = HOGDescriptor.compute(grayImage)
+
+    # Flatten and reshape array
+    features = features.flatten().reshape(1, -1)
+
+    return features
